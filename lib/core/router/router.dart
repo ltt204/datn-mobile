67d4d74032f297cdc69fa39fce31fa71dc4d81c8
ltt204 <trongtin.cv@gmail.com>
2025-09-18 14:48:09 +0700

    project skeleton

---
+++ lib/core/router/router.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:datn_mobile/core/router/router.gr.dart';
+
+/// This class used for defined routes and paths na dother properties
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  late final List<AutoRoute> routes = [
+    AutoRoute(
+      page: CounterRoute.page,
+      path: '/',
+      initial: true,
+    ),
+  ];
+}
---
+++ lib/features/splash/controller/future_initializer.dart
@@ -0,0 +1,66 @@
+import 'package:datn_mobile/shared/riverpod_ext/riverpod_observer/riverpod_obs.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:datn_mobile/i18n/strings.g.dart';
+import 'package:datn_mobile/shared/pods/internet_checker_pod.dart';
+import 'package:datn_mobile/shared/pods/translation_pod.dart';
+import 'package:platform_info/platform_info.dart';
+import 'package:datn_mobile/bootstrap.dart';
+import 'package:datn_mobile/core/local_storage/app_storage_pod.dart';
+import 'package:datn_mobile/features/splash/controller/box_encryption_key_pod.dart';
+import 'package:datn_mobile/init.dart';
+
+final futureInitializerPod =
+    FutureProvider.autoDispose<ProviderContainer>((ref) async {
+  ///Additional intial delay duration for app
+  // await Future.delayed(const Duration(seconds: 1));
+  await (init());
+  await Hive.initFlutter();
+
+  ///Replace `appBox` namw with any key you want
+
+  final encryptionCipher = await Platform.I.when(
+    mobile: () async {
+      final encryptionKey = await ref.watch(boxEncryptionKeyPod.future);
+      return HiveAesCipher(encryptionKey);
+    },
+  );
+
+  ///Load device translations
+  ///
+  AppLocale deviceLocale = AppLocaleUtils.findDeviceLocale();
+  final translations = await deviceLocale.build();
+
+  ///TODO: Replace box name with your unique name
+  final appBox = await Hive.openBox(
+    'AppBox',
+    encryptionCipher: encryptionCipher,
+  );
+  return ProviderContainer(
+    overrides: [
+      appBoxProvider.overrideWithValue(appBox),
+      translationsPod.overrideWith(
+        (ref) => translations,
+      ),
+      enableInternetCheckerPod.overrideWithValue(false),
+    ],
+    observers: [
+      ///Added new talker riverpod observer
+      ///
+      /// If you want old behaviour
+      /// Replace with
+      ///
+      ///  MyObserverLogger( talker: talker,)
+      ///
+      ///
+      ///
+      ///
+      TalkerRiverpodObserver(
+        talker: talker,
+        settings: const TalkerRiverpodLoggerSettings(
+          printProviderDisposed: true,
+        ),
+      ),
+    ],
+  );
+});
