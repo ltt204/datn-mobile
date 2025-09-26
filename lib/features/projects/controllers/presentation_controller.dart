e661621f9d641a460644cd5b81cf13cadda6b5f1
ltt204 <trongtin.cv@gmail.com>
2025-09-26 11:35:50 +0700

    chore: small refactor

--- lib/features/projects/controllers/presentation_controller.dart
+++ lib/features/projects/controllers/presentation_controller.dart
@@ -1,6 +1,5 @@
 part of 'controller_provider.dart';
 
-// Query
 class PresentationsController extends AsyncNotifier<List<PresentationMinimal>> {
   @override
   Future<List<PresentationMinimal>> build() async {
@@ -20,7 +19,6 @@ class PresentationsController extends AsyncNotifier<List<PresentationMinimal>> {
   }
 }
 
-// Command
 class CreatePresentationController extends AsyncNotifier<void> {
   @override
   Future<void> build() async {}
@@ -28,8 +26,10 @@ class CreatePresentationController extends AsyncNotifier<void> {
   Future<void> create(Presentation presentation) async {
     state = const AsyncLoading();
     state = await AsyncValue.guard(() async {
-      await ref.read(presentationServiceProvider).addPresentation(presentation);
-      await ref.read(presentationsControllerProvider.notifier).refresh();
+      await ref
+          .read(createPresentationControllerProvider.notifier)
+          .create(presentation);
+      ref.invalidate(presentationsControllerProvider);
     });
   }
 }
