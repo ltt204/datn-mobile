0ea3e249fb2ff5a274556ad893bd20040bf59954
ltt204 <trongtin.cv@gmail.com>
2025-09-26 09:22:19 +0700

    chore: rename resources to projects

--- lib/features/projects/data/source/resource_remote_source_provider.dart
+++ lib/features/projects/data/source/resource_remote_source_provider.dart
@@ -1,4 +1,4 @@
-import 'package:datn_mobile/features/resources/data/source/resource_remote_source.dart';
+import 'package:datn_mobile/features/projects/data/source/resource_remote_source.dart';
 import 'package:datn_mobile/shared/api_client/dio/dio_client_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
